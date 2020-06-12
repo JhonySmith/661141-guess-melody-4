843d2410e06c7594487e7edc731cbb4753785ada
jhonysmith <evesnin68@gmail.com>
2020-06-12 13:00:30 +0300

    2.2.1 Установит jest, добавит snapshot тесты

---
+++ src/components/welcome-screen/welcome-screen.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import WelcomeScreen from "./welcome-screen.jsx";
+
+describe(`Welcome screen unit tests`, () => {
+  it(`Render Welcome screen`, () => {
+    const tree = renderer
+      .create(
+          <WelcomeScreen
+            errorsCount={3}
+          />)
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
